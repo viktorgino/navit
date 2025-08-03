3990c79a120d65de687cc7ecbcf8f35c80cd4eb8
Viktor Verebelyi <vviktor2@gmail.com>
2025-08-03 18:49:18 +0100

    Vehicle loading

--- navit/plugin_loader.cpp
+++ navit/plugin_loader.cpp
@@ -30,11 +30,54 @@ void PluginLoader::loadPlugins(QList<NavitPluginConfig> &plugins)
 
 void PluginLoader::loadVehicles(QList<NavitVehicleConfig> &vehicles)
 {
+    bool vehicleFound = false;
+
     struct attr *a = g_new0(struct attr, 0);
+    struct attr **vehicleAttrs = NULL;
+
+    struct attr *navit = g_new0(struct attr, 1);
+    struct attr *profilename = g_new0(struct attr, 1);
+    struct attr *source = g_new0(struct attr, 1);
+    struct attr *name = g_new0(struct attr, 1);
+    struct attr *follow = g_new0(struct attr, 1);
+    struct attr *active = g_new0(struct attr, 1);
+
+    navit->type = attr_navit;
+    profilename->type = attr_profilename;
+    source->type = attr_source;
+    name->type = attr_name;
+    follow->type = attr_follow;
+    active->type = attr_active;
+
+    navit->u.navit = m_navit;
+
     for (const NavitVehicleConfig &vehicle : vehicles)
     {
-        vehicle_new(NULL);
+        if (vehicle.active)
+        {
+            vehicleFound = true;
+            profilename->u.str = vehicle.profilename.toLocal8Bit().data();
+            source->u.str = vehicle.source.toLocal8Bit().data();
+            name->u.str = vehicle.name.toLocal8Bit().data();
+            follow->u.num = vehicle.follow;
+            active->u.num = vehicle.active;
+        }
     }
+
+    if (!vehicleFound)
+    {
+        qWarning() << "No active vehicle found!";
+        return;
+    }
+
+    vehicleAttrs = attr_generic_set_attr(vehicleAttrs, navit);
+    vehicleAttrs = attr_generic_set_attr(vehicleAttrs, profilename);
+    vehicleAttrs = attr_generic_set_attr(vehicleAttrs, source);
+    vehicleAttrs = attr_generic_set_attr(vehicleAttrs, name);
+    vehicleAttrs = attr_generic_set_attr(vehicleAttrs, follow);
+    vehicleAttrs = attr_generic_set_attr(vehicleAttrs, active);
+
+    vehicle_new(NULL, vehicleAttrs);
 }
 
 void PluginLoader::loadTracking(NavitTrackingConfig &tracking)
